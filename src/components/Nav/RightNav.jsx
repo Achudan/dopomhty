c29235ea3cea40f6a8fb160d2ba6f0605472da00
Achudan <achuts4@gmail.com>
2022-03-13 10:27:47 -0400

    hidden deceased

--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -42,7 +42,12 @@ const Ul = styled.ul`
 const RightNav = ({ open, currentUser }) => {
   return (
     <Ul open={open}>
-      <li><Link className="option" to="/decesedregistry">Deceased Registry</Link></li>
+      {
+                currentUser?
+                (<li><Link className="option" to="/decesedregistry">Deceased Registry</Link></li>):
+                (<></>)
+            }
+      {/* <li><Link className="option" to="/decesedregistry">Deceased Registry</Link></li> */}
       <li><Link className="option" to="/safelist">Find your pal</Link></li>
       {/* <li><Link className="option" to="/signin">Sign In/Up</Link></li> */}
       {
