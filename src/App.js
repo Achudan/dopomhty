40500f4cc65147d70fc9387335e7572d7f42de25
Achudan <achuts4@gmail.com>
2022-03-12 21:50:20 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import SOS from './pages/SOS/sos.component';
+import Navbar from './components/Nav/Navbar';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      {/* <Navbar></Navbar> */}
+      <Navbar/>
       <SOS/>
+      <Routes>
+        <Route path='/' element={<RecipePage/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/ingredients/:ingredientName' element={<IngredientsPage/>} />
+      </Routes>
     </div>
   );
 }
